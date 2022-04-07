2c9168f46c79c21ebfaf2f2f9537fe7dee930179
hungcuiga1 <44896903+hungcuiga1@users.noreply.github.com>
2022-04-07 22:13:15 +0700

    Add files via upload

---
+++ app/src/androidTest/java/com/robertohuertas/endless/ScreenCaptureServiceTest.java
@@ -0,0 +1,33 @@
+package com.robertohuertas.endless;
+
+import junit.framework.TestCase;
+
+public class ScreenCaptureServiceTest extends TestCase {
+
+    public void testGetStartIntent() {
+    }
+
+    public void testGetStopIntent() {
+    }
+
+    public void testDeleteRecursive() {
+    }
+
+    public void testOnBind() {
+    }
+
+    public void testOnCreate() {
+    }
+
+    public void testOnStartCommand() {
+    }
+
+    public void testGetStatusBarHeight() {
+    }
+
+    public void testGetNavigationBarHeight() {
+    }
+
+    public void testListFilesForFolder() {
+    }
+}
